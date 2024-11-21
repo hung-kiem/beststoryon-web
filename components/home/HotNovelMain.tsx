d7ff46e54bbeb8795b9677a4816f28162bd6ddc2
hung_kiem <kiemhung.socialnetwork@gmail.com>
2024-11-21 20:41:02 +0700

    [DEV][DONE] update url avatar

--- components/home/HotNovelMain.tsx
+++ components/home/HotNovelMain.tsx
@@ -29,9 +29,14 @@ export function StorySummary({ summaryContent }: { summaryContent: string }) {
 type ChapterHotProps = {
   storyName: string;
   numberOfChapter: number;
+  urlAvatar?: string;
 };
 
-function ChapterHot({ storyName, numberOfChapter }: ChapterHotProps) {
+function ChapterHot({
+  storyName,
+  numberOfChapter,
+  urlAvatar,
+}: ChapterHotProps) {
   return (
     <Stack
       direction="row"
@@ -53,7 +58,7 @@ function ChapterHot({ storyName, numberOfChapter }: ChapterHotProps) {
       >
         <CardMedia
           component="img"
-          image="https://plus.unsplash.com/premium_photo-1682125773446-259ce64f9dd7?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          image={urlAvatar || ""}
           alt="Novel image"
           sx={{
             height: "100%",
@@ -288,6 +293,7 @@ export function HotNovelMain({ data }: HotTopListProps) {
                       key={id}
                       storyName={story.storyName}
                       numberOfChapter={story.chapterNumber}
+                      urlAvatar={story.urlAvatar}
                     />
                   </Link>
                 ))}
