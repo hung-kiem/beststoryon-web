ce1d343e4f1bd9d6204b01615f886f0c09d65ee8
hungnk1 <hungnk1@vnpay.vn>
2024-11-28 21:42:04 +0700

    [DEV][DONE] change default image

--- components/trending/TrendingPage.tsx
+++ components/trending/TrendingPage.tsx
@@ -181,10 +181,7 @@ export function TrendingPage() {
                             rating={story.likeCount}
                             status={story.status}
                             chapterNumber={story.chapterNumber}
-                            urlAvatar={
-                              story.urlAvatar ||
-                              "https://plus.unsplash.com/premium_photo-1682125773446-259ce64f9dd7?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                            }
+                            urlAvatar={story.urlAvatar}
                           />
                         </Link>
                       </Grid>
