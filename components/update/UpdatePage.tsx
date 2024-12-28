10d7d96769353350d18f4f32fa3e13a212029868
hungnk1 <hungnk1@vnpay.vn>
2024-12-28 17:02:14 +0700

    [BUG][DONE] filter and sort in hot page, trending page and new release page

--- components/update/UpdatePage.tsx
+++ components/update/UpdatePage.tsx
@@ -37,14 +37,12 @@ export function UpdatePage({
   ) => {
     router.push({
       pathname: `/update/${catCode}/list-${value}.html`,
-      query: router.query,
     });
   };
 
   const handleCategoryClick = (code: string) => {
     router.push({
       pathname: `/update/${code}/list-1.html`,
-      query: router.query,
     });
   };
 
